bb25f680a523e51232a136f61de3b0a2267b0448
Nuno Pereira <nunocgpereira@gmail.com>
2020-05-14 22:23:39 +0100

    style: change background and fonts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.flis5-1.fna.fbcdn.net/v/t51.2885-19/s150x150/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_ohc=ge4mrh4zMFAAX8sQZ7e&oh=c08e7b9db5c9838c5360096a0af1ff57&oe=5EE5AAC6" alt="" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-3"><strong>153</strong> posts</div>
+                <div class="pr-3"><strong>23k</strong> followers</div>
+                <div class="pr-3"><strong>212</strong> following</div>
             </div>
         </div>
     </div>
