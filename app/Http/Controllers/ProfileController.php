2cc43840e91bda47935242e74e676b0fd74830fd
Nuno Pereira <nunocgpereira@gmail.com>
2020-05-14 23:50:56 +0100

    feat: add ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    // Show the application dashboard.
+    public function index()
+    {
+        return view('home');
+    }
 }
