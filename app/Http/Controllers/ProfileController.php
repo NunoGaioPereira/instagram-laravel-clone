19587a357a52587d93f2e9f38e8d9bfe71ab9bca
Nuno Pereira <nunocgpereira@gmail.com>
2020-05-17 22:26:01 +0100

    feat: add default profile image

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -46,12 +46,12 @@ class ProfileController extends Controller
             $imagePath = request('image')->store('profile', 'public');
             $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
             $image->save();
+            $imageArray = ['image' => $imagePath];
         }
         
-
         auth()->user()->profile->update(array_merge(
             $data, 
-            ['image' => $imagePath]
+            $imageArray ?? [] // if empty wont override
         ));
         return redirect("/profile/{$user->id}");
 
