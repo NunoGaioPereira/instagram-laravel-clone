33662e4099edcfc918b48d6828300082e87e6229
Nuno Pereira <nunocgpereira@gmail.com>
2020-05-18 00:01:50 +0100

    feat: add follow button and functionality

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::post('/follow/{user}', 'FollowsController@store');
+
 Route::get('/p/create', 'PostsController@create');
 Route::post('/p', 'PostsController@store');
 Route::get('/p/{post}', 'PostsController@show'); // has to be after /p/create route or get 404
